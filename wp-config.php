2fd22a6c3c4a3e0c4456b2b428867ca482bbf3e0
Martin Genev <mgenev@gmail.com>
2014-07-03 14:07:44 -0700

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -14,16 +14,6 @@
  * @package WordPress
  */
 
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define('DB_NAME', 'hannasta_wrd1');
-
-/** MySQL database username */
-define('DB_USER', 'hannasta_wrd1');
-
-/** MySQL database password */
-define('DB_PASSWORD', '21Ik64Xbvv');
-
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
 
